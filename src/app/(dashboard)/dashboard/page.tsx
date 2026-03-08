8104a7b8bceff0b47083a8f9d4ebc6cacf541f5a
EmanRadwan114 <emanalaa11499@gmail.com>
2026-03-08 10:28:21 +0200

    feat: add chart for products stock

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,12 @@
-export default function DashboardPage() {
-  return <div className="w-full bg-white">hi</div>;
+import ProductBarChart from "./components/ProductBarChart";
+import { getProducts } from "../products/services/products.services";
+
+export default async function DashboardPage() {
+  const productsData = await getProducts();
+
+  return (
+    <div className="w-full">
+      <ProductBarChart products={productsData.products} />
+    </div>
+  );
 }
